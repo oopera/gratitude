46860979c4f811c3eedc85eeda9e539bde2dee34
Oopera <ll@lucaslichner.de>
2024-11-20 16:31:18 +0100

    add cookie model id for admin users

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components/custom/navbar";
 import { getUser } from "@/db/queries";
 import { DEFAULT_MODEL_NAME, models } from "@/lib/ai/models";
 import { generateUUID } from "@/lib/utils";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "../(auth)/auth";
 
@@ -21,17 +22,24 @@ export default async function Page() {
   }
 
   let selectedModelId = DEFAULT_MODEL_NAME;
+  const cookieStore = await cookies();
+  const modelIdFromCookie = cookieStore.get("model-id")?.value;
+
+  const cookieModelId =
+    models.find((model) => model.id === modelIdFromCookie)?.id ||
+    DEFAULT_MODEL_NAME;
 
   const modelMapping: Record<string, string> = {
     condition_one: "condition_one",
     condition_two: "condition_two",
     control: "control",
+    admin: cookieModelId,
   };
 
   selectedModelId =
     models.find((model) => model.id === modelMapping[userType])?.id ??
     DEFAULT_MODEL_NAME;
-  console.log(selectedModelId);
+
   return (
     <>
       <Navbar userType={userType} selectedModelId={selectedModelId} />
