1dd8100967701c8ea366eb7566497f5c065dce12
Oopera <ll@lucaslichner.de>
2024-12-19 00:06:47 +0100

    add pw placeholder

--- components/custom/auth-form.tsx
+++ components/custom/auth-form.tsx
@@ -41,6 +41,7 @@ export function AuthForm({
         <Input
           id="password"
           name="password"
+          placeholder="Passwort"
           className="bg-muted text-md md:text-sm"
           type="password"
           required
